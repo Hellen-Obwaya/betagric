dbf986a5f4fe6a136adec1fea3e5fd84e5236da9
Hellen-Obwaya <hadhiambo5@gmail.com>
2023-03-24 23:03:00 +0300

    update events_card

--- lib/widgets/events_card.dart
+++ lib/widgets/events_card.dart
@@ -1,49 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+List upcoming_event = [
+  "Physical",
+  "Tiktok",
+  "Facebook",
+  "Twitter",
+  "Physical",
+];
+List location_link = [
+  "Nairobi",
+  "Muranga",
+  "Kirinyaga",
+  "Embu",
+  "Makueni",
+];
 
 class EventsCard extends StatelessWidget {
   const EventsCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'Event Name',
-              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 5.0),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Enter event name',
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 212, 242, 178),
+      appBar: AppBar(
+        title: const Text(
+          "Upcoming Events",
+          style: TextStyle(
+              color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 41, 180, 73),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(5),
+                bottomRight: Radius.circular(5),
+                topLeft: Radius.circular(5),
+                topRight: Radius.circular(5))),
+      ),
+      body: ListView.builder(
+        itemCount: 5,
+        shrinkWrap: true,
+        itemBuilder: (BuildContext, int index) => Container(
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 0.5),
+          child: Card(
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0.0)),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 0.5),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          height: 55.0,
+                          width: 55.0,
+                          color: Colors.grey,
+                          child: CircleAvatar(
+                            backgroundColor: Colors.grey,
+                            backgroundImage: AssetImage("images/twitter.jpg"),
+                          ),
+                        ),
+                        SizedBox(width: 0.5),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              upcoming_event[index],
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20),
+                            ),
+                            Text(
+                              location_link[index],
+                              style:
+                                  TextStyle(color: Colors.grey, fontSize: 18),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: Get.width * .20,
+                    height: 30,
+                    child: ElevatedButton(
+                      child: Text("Schedule".toLowerCase(),
+                          style: TextStyle(fontSize: 12)),
+                      style: ButtonStyle(
+                          foregroundColor:
+                              MaterialStateProperty.all<Color>(Colors.white),
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Color.fromARGB(255, 41, 180, 73)),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(5),
+                                      side: BorderSide(
+                                        color:
+                                            Color.fromARGB(255, 201, 193, 193),
+                                      )))),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
               ),
             ),
-            SizedBox(height: 16.0),
-            Text(
-              'Location/Link',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8.0),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Enter location',
-              ),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Contact Person',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8.0),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Enter contact person',
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
